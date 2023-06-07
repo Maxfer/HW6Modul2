c158597f90018c1f77958d6e7f4a1a14ef8c9dcb
Maxfer <maximus604@gmail.com>
2023-06-07 14:15:14 +0300

    Initial Commit

---
+++ HW6Modul2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6Modul2
+//
+//  Created by Максим Шкрябин on 07.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
